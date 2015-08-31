b355dc2960bd4cf5b604eae72e8e66d7cc106702
Mauro Scomparin <scompo@gmail.com>
2015-08-31 22:25:21 +0200

    addded rollback

--- src/test/java/com/github/scompo/testsdn4j/ApplicationContextTest.java
+++ src/test/java/com/github/scompo/testsdn4j/ApplicationContextTest.java
@@ -4,10 +4,15 @@ import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @PersistenceContextTest
+@Transactional(propagation = Propagation.REQUIRED)
+@Rollback(value = true)
 public class ApplicationContextTest {
 
 	@Test
