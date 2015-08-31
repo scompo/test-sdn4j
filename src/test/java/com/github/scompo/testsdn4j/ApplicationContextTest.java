6dafc200c1f158297c03b278dce14f24463b59db
Mauro Scomparin <scompo@gmail.com>
2015-08-31 21:44:34 +0200

    added test annotation

--- src/test/java/com/github/scompo/testsdn4j/ApplicationContextTest.java
+++ src/test/java/com/github/scompo/testsdn4j/ApplicationContextTest.java
@@ -1,19 +1,18 @@
 package com.github.scompo.testsdn4j;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = Application.class)
+@MyIntegrationTests
 public class ApplicationContextTest {
 
 	@Test
 	public void shouldStart() {
 
-		assertTrue("not started", true);
+		assertTrue(true);
 	}
 }
