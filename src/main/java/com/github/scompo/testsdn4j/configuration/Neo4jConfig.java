4ea537c08f53ddf3ddde0e32eac669743c8c7786
Mauro Scomparin <scompo@gmail.com>
2015-09-01 02:29:47 +0200

    wip

--- src/main/java/com/github/scompo/testsdn4j/configuration/Neo4jConfig.java
+++ src/main/java/com/github/scompo/testsdn4j/configuration/Neo4jConfig.java
@@ -1,18 +1,23 @@
 package com.github.scompo.testsdn4j.configuration;
 
 import org.neo4j.ogm.session.SessionFactory;
+import org.springframework.batch.core.configuration.annotation.SimpleBatchConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.neo4j.config.Neo4jConfiguration;
 import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
 import org.springframework.data.neo4j.server.Neo4jServer;
 import org.springframework.data.neo4j.server.RemoteServer;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.data.transaction.ChainedTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
 
 @Configuration
 @EnableNeo4jRepositories(basePackages = "com.github.scompo.testsdn4j.repositories")
-@EnableTransactionManagement
 public class Neo4jConfig extends Neo4jConfiguration {
+
+	@Autowired
+	private SimpleBatchConfiguration batchConfigurer;
 
 	public Neo4jConfig() {
 
@@ -32,6 +37,13 @@ public class Neo4jConfig extends Neo4jConfiguration {
 	public Neo4jServer neo4jServer() {
 
 		return new RemoteServer("http://localhost:7474");
+	}
+
+	@Override
+	@Bean(name = "transactionManager")
+	public PlatformTransactionManager transactionManager() throws Exception {
+
+		return new ChainedTransactionManager(batchConfigurer.transactionManager(), super.transactionManager());
 	}
 
 }
